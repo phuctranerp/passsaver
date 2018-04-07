0161b0233994f431949e4f6b27cbd4c3f4e36f61
Tran Thanh Phuc <phuc@trobz.com>
2018-04-08 00:13:29 +0700

    [ADD] - Setup login page

---
+++ src/components/Login/login.js
@@ -0,0 +1,20 @@
+/**
+ * Login form components
+ */
+
+import React, {Component} from "react";
+
+import {
+    View, Text
+} from "react-native";
+
+
+export default class Login extends React.Component{
+    render() {
+        return (
+            <View>
+                <Text>This is Login form</Text>
+            </View>
+        );
+    }
+} 
